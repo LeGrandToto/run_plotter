3cba0bc9a995e4ece48ea08911dad9f633d2596e
LeGrandToto <amberg.marc@gmail.com>
2023-09-24 17:29:48 +0100

    Allowed to import multiple GPX file (one track expected per file)

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
+from folium.folium import validate_location
 from gpx.waypoint import Waypoint
 import panel as pn
 import folium
+from param import Event
 import plotly.express as px
 from gpx.gpx import GPX
 from math import sqrt
 
-def create_folium(file_paths: list[str]):
-    if not file_paths:
-        return "No track found!"
+import logging
 
-    tracks = [GPX.from_file(file_path).tracks[0] for file_path in file_paths]
+logger = logging.getLogger(__name__)
 
+def create_folium(tracks: list[GPX]):
     tiles = ["OpenStreetMap", "CartoDB positron", "Stamen Terrain", "Stamen Watercolor", "CartoDB dark_matter"]
     default_tile_index = 2
     
@@ -38,27 +39,57 @@ def create_speed_plots(file_paths):
         time = next_loc.time - current.time
         return distance / (time.total_seconds() / 3600)
     plots = []
-    for file_path in file_paths:
+    for index, file_path in enumerate(file_paths):
         track = GPX.from_file(file_path).tracks[0]
 
         speeds = [compute_speed(c, n ) for c, n in zip(track.segments[0].points, track.segments[0].points[1:])]
         surface = px.line(x= range(len(speeds)), y= speeds)
         surface.layout.xaxis["title"] = {"text": "Time"}
         surface.layout.yaxis["title"] = {"text": "Speed in km/h"}
-        plots.append(pn.pane.Plotly(surface))
-    return pn.Column(*plots)
+        plotly_object = pn.pane.Plotly(surface)
+        plotly_object.index = index
+        plots.append(plotly_object)
+    return pn.FlexBox(*plots)
 
+marker: folium.Marker = None
 def main(track_files):
-    folium_map = create_folium(track_files)
+    if not track_files:
+        return "No track found!"
+    tracks = [GPX.from_file(file_path).tracks[0] for file_path in track_files]
+    folium_map = create_folium(tracks)
     speed_plots = create_speed_plots(track_files)
 
     template = pn.template.BootstrapTemplate(
             title= "Run Plotter",
             main= [
-                    folium_map,
+                    pn.Column(folium_map,),
                     speed_plots,
                 ],
             )
+
+    def speed_plot_callback(event: Event):
+        global marker
+        print(event)
+        if event.name == "hover_data":
+            if event.new:
+                for track in event.new["points"]:
+                    # track_index = track["curveNumber"]
+                    track_index = event.cls.index
+                    segment_index = track["pointIndex"]
+                    selected_segment = tracks[track_index].segments[0].points[segment_index]
+                    # pprint(dir(folium_map.object))
+                    if marker:
+                        marker.location = validate_location([selected_segment.lat, selected_segment.lon])
+                    else:
+                        marker = folium.Marker(
+                                location= [selected_segment.lat, selected_segment.lon],
+                                icon= folium.Icon()
+                            )
+                        marker.add_to(folium_map.object)
+                    template.main[0][0] = pn.pane.plot.Folium(folium_map.object, height= 800)
+
+    for speed_plot in speed_plots:
+        speed_plot.param.watch(speed_plot_callback, ["hover_data"])
 
     return template
 
